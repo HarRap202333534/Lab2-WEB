80dfdb6dd6cb28e6abf12a8c5d156c51f7aa9493
2333534 <raphaelharvey15@gmail.com>
2025-02-27 19:32:13 -0500

    Added Jquery dialog

--- script/createPost.js
+++ script/createPost.js
@@ -5,25 +5,41 @@ async function getPosts() {
 }
 
 async function createPost(contenu) {
-    const id = JSON.stringify((await getPosts()).length + 1);
-    let date = new Date();
-    const now = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay();
-    const form = document.forms["postForm"];
-    const title = form["title"].value;
-    const author = form["author"].value;
-    const content = contenu
+    $('#dialog').dialog({
+        modal: true,
+        title: "Confirmation",
+        open: function(){
+            $(this).text("Voulez-vous vraiment publier ce post ?");
+        },
+        buttons: {
+            "Oui": async function() {
+                $(this).dialog("close");
+                const id = JSON.stringify((await getPosts()).length + 1);
+                let date = new Date();
+                const now = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay();
+                const form = document.forms["postForm"];
+                const title = form["title"].value;
+                const author = form["author"].value;
+                const content = contenu
 
-    fetch('http://localhost:3000/publications', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
+                fetch('http://localhost:3000/publications', {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8'
+                        },
+                        body: JSON.stringify({
+                            "id": id,
+                            "title": title,
+                            "author": author,
+                            "datePublication": now,
+                            "contenu": content
+                        })
+                });
+                window.location.href = "index.html";
             },
-            body: JSON.stringify({
-                "id": id,
-                "title": title,
-                "author": author,
-                "datePublication": now,
-                "contenu": content
-            })
-        });
+            "Non": function() {
+                $(this).dialog("close");
+            }
+        },
+    }, "open");
 }
